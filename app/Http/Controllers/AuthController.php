99a9a7a9bde7d8d5afa97ac8859d80a177146527
dungvnph06543 <rungruoiit99@gmail.com>
2019-11-05 09:56:51 +0700

    register app

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\User;
 use Illuminate\Support\Facades\Lang;
+use App\Http\Requests\RegisterRequest;
 
 class AuthController extends Controller
 {
