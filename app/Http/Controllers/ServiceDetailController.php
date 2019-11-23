bada54d107e74ac7b385a0f61c17089670afe5d8
TrungPaul <trungmadrid@gmail.com>
2019-11-23 21:32:54 +0700

    up

---
+++ app/Http/Controllers/ServiceDetailController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\UploadImageService;
+use Illuminate\Http\Request;
+use App\User;
+use App\ServiceDetail;
+use Illuminate\Support\Facades\Auth;
+
+class ServiceDetailController extends Controller
+{
+    public function __construct(UploadImageService $uploadService)
+    {
+        $this->uploadService = $uploadService;
+    }
+
+    public function index($spaId)
+    {
+
+        $service = ServiceDetail::where('spa_id', $spaId)->get();
+
+        return view('pages-spa.ListServiceDetail', compact( 'service' , 'spaId'));
+    }
+
+    public function destroy($id)
+    {
+        ServiceDetail::where('id', $id)->delete();
+
+        return $this->index(Auth::user()->id);
+    }
+
+    public function getAdd($spaId)
+    {
+
+        return view('pages-spa.addServiceDetail', compact('spaId'));
+    }
+
+    public function postAddServiceDetail(Request $request)
+    {
+        $this->validate($request, [
+            'name_service' => 'required',
+            'spa_id' => 'required',
+            'service_id'=> 'required',
+          'price_service' => 'required',
+          'discount' => 'nullable',
+        'detail_service' => 'required',
+        'image_service' => 'required'
+        ]);
+        $serviceDetail = new ServiceDetail();
+        $serviceDetail->fill($request->all());
+        if ($request->hasFile('image_service')) {
+            $avatar = $request->image_service;
+            $serviceDetail->image_service = $this->uploadService->uploadFile($avatar);
+        }
+        $serviceDetail->save();
+
+        return $this->index($request->spa_id);
+    }
+
+    public function getUpdate($id)
+    {
+        $service = ServiceDetail::where('id', $id)->get();
+
+        return view('pages-spa.updateServiceDetail', compact('service'));
+    }
+
+    public function update(Request $request ,$id)
+    {
+        $this->validate($request, [
+            'name_service' => 'required',
+            'spa_id' => 'required',
+            'service_id'=> 'required',
+            'price_service' => 'required',
+            'discount' => 'nullable',
+            'detail_service' => 'required',
+            'image_service' => 'nullable'
+        ]);
+        $serviceDetail = ServiceDetail::find($id);
+        $serviceDetail->fill($request->all());
+        $serviceDetail->spa_id = Auth::user()->id;
+        if ($request->hasFile('image_service')) {
+            $avatar = $request->image_service;
+            $serviceDetail->image_service = $this->uploadService->uploadFile($avatar);
+        }
+        $serviceDetail->update();
+
+        return $this->index($serviceDetail->spa_id);
+    }
+}
